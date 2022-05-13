6130306ce8fa7ff210d2e0f7f02ddea8dfbbe990
Graciana <gracianaaad@hotmail.com>
2022-05-13 11:28:32 +0200

    add all CheckBoxes to CheckBoxes component

---
+++ src/components/main/CheckBoxes.js
@@ -0,0 +1,45 @@
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import CustomCheckbox from './CustomCheckBox';
+
+
+const CheckBoxes=(props)=>{
+
+    return(
+        <>
+        {props.objectQuestions
+        .slice(props.page * props.rowsPerPage, props.page * props.rowsPerPage + props.rowsPerPage)
+        .map((row) => (
+          <TableRow key={row.id} hover>
+            <TableCell component="th" scope="row">
+              {row.position+1}. {row.question}
+            </TableCell>
+            {props.objectStudents.map((student) => (
+              <TableCell
+                align="right"
+              >
+                <CustomCheckbox
+                  studentId={student.student}
+                  questionId={row.id}
+                  objectStudents={props.objectStudents}
+                  setObjectStudents={props.setObjectStudents}
+                  questionStudent={props.questionStudent}
+                  setQuestionStudent={props.setQuestionStudent}
+                />
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+        </>
+    )
+
+
+
+
+
+}
+
+
+
+
+export default CheckBoxes;
