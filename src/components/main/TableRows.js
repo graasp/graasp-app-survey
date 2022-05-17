7b5c59d342038881601330865baf43d9e03e8ab7
Graciana <gracianaaad@hotmail.com>
2022-05-17 17:15:14 +0200

    iterate over questionStudet instead of studentObject

--- src/components/main/TableRows.js
+++ src/components/main/TableRows.js
@@ -2,47 +2,40 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CustomCheckbox from './CustomCheckBox';
 
-
-
-const TableRows=(props)=>{
-
-
-    return(
-        <>
-        {props.objectQuestions
-        .slice(props.page * props.rowsPerPage, props.page * props.rowsPerPage + props.rowsPerPage)
-        .map((row) => (
-          <TableRow key={row.id} hover>
+const TableRows = (props) => {
+  const hey = props.questionStudent.filter(({ data }) => data.questionId);
+  console.log('heyy', hey);
+  return (
+    <>
+      {props.objectQuestions
+        .slice(
+          props.page * props.rowsPerPage,
+          props.page * props.rowsPerPage + props.rowsPerPage,
+        )
+        .map((question) => (
+          <TableRow key={question.id} hover>
             <TableCell component="th" scope="row">
-              {row.position+1}. {row.question}
+              {question.position + 1}. {question.question}
             </TableCell>
-            {props.objectStudents.map((student,index) => (
-              <TableCell
-                align="right"
-                key={index}
-              >
-                <CustomCheckbox
-                  studentId={student.student}
-                  questionId={row.question}
-                  objectStudents={props.objectStudents}
-                  setObjectStudents={props.setObjectStudents}
-                  questionStudent={props.questionStudent}
-                  setQuestionStudent={props.setQuestionStudent}
-                />
-              </TableCell>
-            ))}
+            {props.questionStudent
+              .filter(({ data }) => data.questionId === question.question)
+              .map((student, index) => (
+                <TableCell align="right" key={index}>
+                  <CustomCheckbox
+                    state={student.data.state}
+                    studentId={student.data.studentId}
+                    questionId={question.question}
+                    objectStudents={props.objectStudents}
+                    setObjectStudents={props.setObjectStudents}
+                    questionStudent={props.questionStudent}
+                    setQuestionStudent={props.setQuestionStudent}
+                  />
+                </TableCell>
+              ))}
           </TableRow>
         ))}
-        </>
-    )
-
-
-
-
-
-}
-
-
-
+    </>
+  );
+};
 
 export default TableRows;
